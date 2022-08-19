74e61e15d0233ae824d2af332cc8384df01973b1
naoyahieda <naoyahieda@gmail.com>
2022-08-19 21:46:35 +0900

    pokeAPI実装

---
+++ pages/poke/[id].js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const PokeDetailPage = ({ pokeData }) => {
+  return (
+    <div>
+      <div className="max-w-2xl mx-auto">
+        <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+          <a>
+            <img
+              className="rounded-t-lg"
+              src={pokeData.sprites.front_default}
+              alt=""
+            />
+          </a>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                名前：{pokeData.name}
+              </h5>
+            </a>
+            <ul className="font-normal text-gray-700 mb-3 dark:text-gray-400">
+              <li>重さ：{pokeData.weight}</li>
+                <li>タイプ：{pokeData.types.map((type) => type.type.name)}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const pokeID = context.params.id;
+  const POKEMON_DETAIL_URL = `https://pokeapi.co/api/v2/pokemon/${pokeID}/`;
+  const res = await fetch(POKEMON_DETAIL_URL);
+  // 404
+    if (res.status === 404) {
+        return {
+            notFound: true
+          };
+    }
+  const pokeData = await res.json();
+  return { props: { pokeData } };
+}
+
+export default PokeDetailPage;
---
+++ pages/poke/index.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
+
+const POKEMON_LIST_URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+
+
+const PokeList = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    axios.get(POKEMON_LIST_URL)
+
+      .then(res => {
+        setPokemonList(res.data.results);
+      }).catch(err => {
+        console.log(err);
+      }
+      );
+  }
+  , []);
+
+  const getIdFromUrl= (url) => {
+    return url.split("/").slice(-2)[0]
+  }
+  
+  return (
+    <div className="w-48 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        {pokemonList.map((pokemon, index) => {
+            const pokemonId = getIdFromUrl(pokemon.url)
+            return (
+                <div key={index}>       
+                <a href={`/poke/${pokemonId}`} aria-current="true" className="block py-2 px-4 w-full text-white bg-blue-700 rounded-t-lg border-b border-gray-200 cursor-pointer dark:bg-gray-800 dark:border-gray-600">
+                    {pokemon.name}
+                </a>
+                </div> 
+            )
+        })}
+    </div>
+  )
+}
+
+
+
+export default PokeList
